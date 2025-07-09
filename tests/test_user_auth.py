65005acdc8ab04054c845390f4ad17d80d542761
Irina Sh. <104162505+Kononirin@users.noreply.github.com>
2025-07-09 20:46:30 +0300

    Add test_negative_auth_check (#6)
    * Add def test_auth_user
    
    * Add json_parsing, try_except
    
    * Refactor code after review 10.06.2025
    
    * Refactor code after review 24.06.2025. Add def response_login in test_user_auth.py.
    
    * Refactor code after review 01.07.2025. Add mistake text in assert.
    
    * Add test_negative_auth_check

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,4 +1,5 @@
 import requests
+import pytest
 
 class TestUserAuth:
     def response_login(self):
@@ -35,5 +36,34 @@ class TestUserAuth:
         user_id_from_check_method = response_auth_json['user_id']
 
         assert 'user_id' in response_auth_json, 'There is no user id in the second response'
-
+        
         assert user_id_from_auth_method == user_id_from_check_method, f"User {user_id_from_auth_method} not equals user {user_id_from_check_method}"
+
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
+
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
+        response_login = self.response_login()
+
+        auth_sid = response_login.cookies.get('auth_sid')
+        token = response_login.headers.get('x-csrf-token')
+
+        if condition == "no_cookie":
+            response_auth = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                headers={'x-csrf-token': token}
+            )
+        else:
+            response_auth = requests.get(
+                "https://playground.learnqa.ru/api/user/auth",
+                cookies={'auth_sid':auth_sid}
+            )
+
+        assert "user_id" in response_login.json(), "There is no user id in the response_auth response"
+
+        user_id_from_check_method = response_auth.json()["user_id"]
+
+        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
